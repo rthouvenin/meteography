ef73524af5a7f11714a89270e7d76c880d1193ce
rthouvenin <romain.thouvenin@gmail.com>
2015-09-22 19:31:56 +0200

    Create setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Meteography",
+    version = "0.1",
+    description = "Aims at predicting what the sky will look like",
+    author = "Romain Thouvenin",
+    author_email = "romain.thouvenin@gmail.com",
+    packages = find_packages(),
+)

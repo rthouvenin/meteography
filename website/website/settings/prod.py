a61199fdfb27aac1d652ae4d55bbf7f754fc287a
rthouvenin <romain.thouvenin@gmail.com>
2016-01-16 11:15:50 +0100

    Add log config to prod settings

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -1,6 +1,26 @@
-from base import *
+import os
 import dj_database_url
+from base import *
 
 DEBUG = False
 
 DATABASES = {'default': dj_database_url.config()}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': True,
+        },
+    },
+}
